067775c02e32613fefddc85078437179db8cb744
vstro kbs <kbs21c1987@gmail.com>
2021-12-31 10:31:23 +0900

    change

--- ieChangeEdge.js
+++ ieChangeEdge.js
@@ -1,9 +1,10 @@
 // IE 스크립트 활성화시 Edge로 브라우저 변경
+// url 부분에 해당 주소 입력
 if(navigator.userAgent.indexOf("Trident") > 0){ 
-    console.log("exLog");
-    window.location = 'microsoft-edge:' + "https://mintway.io";
+    // console.log("exLog");
+    window.location = 'microsoft-edge:' + url;
 }
 else if(/MSIE \d |Trident.*rv:/.test(navigator.userAgent)){
-    console.log("exLog");
+    // console.log("exLog");
     window.location = 'microsoft-edge:' + url;
 }
